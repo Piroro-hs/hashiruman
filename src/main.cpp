38f2dff13ea76ccd6c6ed887a7f9873edbe73112
Piroro-hs <Piroro-hs@users.noreply.github.com>
2018-07-08 20:19:45 +0900

    Pwm

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <mbed.h>
+
+namespace {
+  Serial pc(USBTX, USBRX);
+  PwmOut edf(PA_6);
+  float speed = 0;
+}
+
+int main(){
+  wait(0.5);
+  pc.printf("Start\n");
+  while (true) {
+    while (pc.readable()){
+      pc.scanf("%f", &speed);
+      pc.printf("Set %f\n", speed);
+      edf.write(speed);
+    }
+  }
+}
